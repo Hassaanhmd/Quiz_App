6a13799d1f5d6e179b557b729cc2722fa2408e9d
Hazey <hassaanhmd@gmail.com>
2022-08-19 23:37:38 +0200

    used new widget to make app look nicer (Learning about widget trees)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() =>
+    runApp(MyApp()); // shorthand for function with only one expression
 
 class MyApp extends StatelessWidget {
+  @override // "decorator" - to show that you are DELIBERATELY overwriting the method
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First App')),
+        body: Text('This is my default text!'),
+      ),
     );
   }
 }
