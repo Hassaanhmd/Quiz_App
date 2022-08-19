4f809e3da647e10a7414fbf981a478585f99b750
Hazey <hassaanhmd@gmail.com>
2022-08-20 00:08:33 +0200

    Added buttons with methods to execute different terminal outputs (basic but learning syntax)

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,41 @@ void main() =>
     runApp(MyApp()); // shorthand for function with only one expression
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override // "decorator" - to show that you are DELIBERATELY overwriting the method
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s youre favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('My First App')),
-        body: Text('This is my default text!'),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ), // RaisedButton() deprecated use ElevatedButton() - only difference is some styling
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // ...
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
