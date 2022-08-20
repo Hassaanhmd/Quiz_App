cca119a0021d3bce578d8b68bbdcffad7a17e47d
Hazey <hassaanhmd@gmail.com>
2022-08-20 10:01:11 +0200

    Added state management for updating questions on answer click

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,22 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(MyApp()); // shorthand for function with only one expression
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer chosen!');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState(); // '_' turns from public to private class/property(variable)/method
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    }); // forces flutter to re-render the UI by calling build of widget where setState() is called
+    print(_questionIndex);
   }
 
   @override // "decorator" - to show that you are DELIBERATELY overwriting the method
@@ -25,10 +38,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(
+              questions[_questionIndex],
+            ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ), // RaisedButton() deprecated use ElevatedButton() - only difference is some styling
             RaisedButton(
               child: Text('Answer 2'),
